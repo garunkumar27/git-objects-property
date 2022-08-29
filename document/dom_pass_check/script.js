f45faefff280e4e8d340b30789cb50ff6456fc66
arun kumar <garunk.27@gmail.com>
2022-08-29 16:15:10 +0530

    DOM Shopping Concept.

---
+++ document/dom_pass_check/script.js
@@ -0,0 +1,11 @@
+
+// function pass(){
+// let x=document.getElementById("password").value;
+// let z=document.getElementById("con_password").value;
+// if(x==z){
+// 	document.getElementById("pas").innerHTML="Password is Correct";
+// }
+// else{
+// document.getElementById("pas").innerHTML="Password is In-correct";
+// }
+// }
