94d2913e4a8d4c96c07fa5a48dbb07d9406049e0
arun kumar <garunk.27@gmail.com>
2022-09-07 20:30:29 +0530

    Dom Radio Button.

---
+++ document/dom_radio/script.js
@@ -0,0 +1,15 @@
+// function rad(){
+// 	let eng=document.getElementById("english");
+// 	let tam=document.getElementById("tamil");
+// 	let mal=document.getElementById("malayalam");
+
+// 	if(eng.checked)
+// 		alert(eng.value);
+// 	else if(tam.checked)
+// 		alert(tam.value);
+// 	else if(mal.checked)
+// 		alert(mal.value);
+// 		else
+// 		alert("Choose Your Language")
+
+// }
