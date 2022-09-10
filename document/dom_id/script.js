eb5f4341867e85c576a1adb4bdba2e1254ca6e01
arun kumar <garunk.27@gmail.com>
2022-09-10 22:31:22 +0530

    Dom.

---
+++ document/dom_id/script.js
@@ -0,0 +1,8 @@
+// Finding Html document
+// 1.document.getElementById(id)
+//It is used to find the elements using Id
+function changes()
+{
+let y=document.getElementById("doc").innerHTML="Cool Buddy";
+alert(y);
+}
